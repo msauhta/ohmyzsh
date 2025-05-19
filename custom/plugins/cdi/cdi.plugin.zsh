65412d47481690a8a9b13db45a3e8a9bc153063f
msauhta <mohit.sauhta@gmail.com>
2025-05-19 16:27:09 -0400

    fix cdi

--- custom/plugins/cdi/cdi.plugin.zsh
+++ custom/plugins/cdi/cdi.plugin.zsh
@@ -15,12 +15,26 @@ compdef _cdi cdi
 #######END OF CDI AUTOCOMPLETE###################################################
 cdi()
 {
-
+    # increase the number $1 by in the .projects file and sort the file
+    # by number followed by project name
+    cat $HOME/.projects |\
+            awk -v project="$1" '{if ($2 == project) {print $1+1 "\t" $2} else {print $1 "\t" $2}}' > /tmp/.list_projects
+    cat /tmp/.list_projects |\
+        sort -k1,1nr -k2,2 |\
+        awk '!seen[$2]++' > $HOME/.projects
+    CDI_PROJECTS=$(cat $HOME/.projects)
     cd "$HOME/code/$1"
 }
 
 cdir()
 {
-    $HOME/go/bin/list_projects > $HOME/.projects
+
+    $HOME/go/bin/list_projects | sed -e 's/^/0\t/' > /tmp/.list_projects
+    if [ -f $HOME/.projects ]; then
+        cat $HOME/.projects >> /tmp/.list_projects
+    fi    
+    cat /tmp/.list_projects |\
+     sort -k1,1nr -k2,2 |\
+      awk '!seen[$2]++' > $HOME/.projects
     CDI_PROJECTS=$(cat $HOME/.projects)
 }
