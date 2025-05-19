9a04592a3f3ffb9a5699edd30dc4e6ce6677c73b
msauhta <mohit.sauhta@gmail.com>
2025-05-19 16:26:12 -0400

    Load pyenv

--- custom/my_profile.zsh
+++ custom/my_profile.zsh
@@ -8,3 +8,11 @@ if which jenv > /dev/null; then eval "$(jenv init -)"; fi
 
 
 export PATH="~/.jenv/shims:$PATH"
+
+
+# Load pyenv automatically
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
